0669df2a6d4ad7ea895a113b2b3290425ac83338
KekiNovA <pushpak.pachpande@gmail.com>
2024-02-19 23:22:38 +0530

    Fixed permission issue.

--- notes/permissions.py
+++ notes/permissions.py
@@ -10,7 +10,7 @@ class IsNoteOwner(permissions.BasePermission):
 class HasNoteAccess(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         mapping = NoteSharedUsers.objects.filter(note=obj)
-        allow_user = mapping.exists() and request.user in mapping.first().shared_user.all()
+        allow_user = mapping.exists() and request.user in mapping.first().shared_users.all()
         return (
             obj.created_by == request.user
             or allow_user
